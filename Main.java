05e14aebe4becd1dcbe41701e647de5785da1c34
Jayesh Shinde <64770565+shindejayesh987@users.noreply.github.com>
2023-02-08 07:02:48 +0000

    .

--- Main.java
+++ Main.java
@@ -1,26 +1,31 @@
 class Main {
   public static void main(String[] args) {
-    int[] nums = {21,65,9,86,5,32,4,58};
-    int target = 21;
-    int ans = LinearSearch(nums , target);
 
-
-    System.out.println(ans);
-   
+    int [] arr = {4,5,6,7,0,1,2};
+    System.out.println(findPivot(arr));
+    
+    
   }
 
-  static int LinearSearch(int[] arr , int target){
-    if(arr.length == 0 ){
-      System.out.print("Item not found!!!!");
-    }
+  static int findPivot(int [] arr){
+    int start =0;
+    int end = arr.length -1 ;
 
-    for(int index = 0; index < arr.length ; index++){
-      int element = arr[index];
-      if(element == target){
-      return index;
+    while(start <= end){
+      int mid = start + (end - start)/2;
+
+      if(mid < end && arr[mid] > arr[mid +1]){
+        return mid;
       }
-
+      if(mid > start && arr[mid] < arr[mid - 1]){
+        return mid -1;
+      }
+      if(arr[mid] <=  arr[start]){
+        end = mid -1 ;
+      }else{
+        start = mid + 1;
+      }
     }
     return -1;
-  }
+  }  
 }
